c821f70273ef628419e9f7b9ad7e83d8040a225a
Andrian <43666275+Andrian1999@users.noreply.github.com>
2018-10-22 10:32:25 +0700

    Tugas Pemrograman berbasis Objek

---
+++ src/Perulangan.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Scanner;
+
+public class Perulangan {
+
+    public static void main(String[] args) {
+        int pilih;
+        Scanner s = new Scanner(System.in);
+        do
+        {
+            System.out.println("1.input data");
+            System.out.println("2.tampil data");
+            System.out.println("3.keluar");
+            System.out.print("pilih : "); pilih = s.nextInt();
+            }while(pilih!=3);
+    }
+    
+}
